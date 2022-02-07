b4eb90f55e5d9c5e1318d472c1b60e36533c240b
vs-calvin <calvin@versusnow.io>
2022-02-07 10:32:48 +0900

    init

--- pages/features.tsx
+++ pages/features.tsx
@@ -2,26 +2,26 @@
 // This file is owned by you, feel free to edit as you see fit.
 import * as React from "react";
 import { ScreenVariantProvider } from "../components/plasmic/simple_light_landing_page/PlasmicGlobalVariant__Screen";
-import { PlasmicHome } from "../components/plasmic/simple_light_landing_page/PlasmicHome";
+import { PlasmicFeatures } from "../components/plasmic/simple_light_landing_page/PlasmicFeatures";
 
-function Home() {
-  // Use PlasmicHome to render this component as it was
+function Features() {
+  // Use PlasmicFeatures to render this component as it was
   // designed in Plasmic, by activating the appropriate variants,
   // attaching the appropriate event handlers, etc.  You
   // can also install whatever React hooks you need here to manage state or
   // fetch data.
   //
-  // Props you can pass into PlasmicHome are:
+  // Props you can pass into PlasmicFeatures are:
   // 1. Variants you want to activate,
   // 2. Contents for slots you want to fill,
   // 3. Overrides for any named node in the component to attach behavior and data,
   // 4. Props to set on the root node.
   //
-  // By default, PlasmicHome is wrapped by your project's global
+  // By default, PlasmicFeatures is wrapped by your project's global
   // variant context providers. These wrappers may be moved to
   // Next.js Custom App component
   // (https://nextjs.org/docs/advanced-features/custom-app).
-  return <PlasmicHome />;
+  return <PlasmicFeatures />;
 }
 
-export default Home;
+export default Features;
